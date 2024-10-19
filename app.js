2d90cf081784fa9c0e757f0c3715b9e5e7a1dfaf
Abhijith s <abhijiths2204@gmail.com>
2024-10-19 17:43:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const cors = require('cors'); // Import cors
+const cors = require('cors'); 
 
 const app = express();
 
-// Middleware
-app.use(cors()); // Enable CORS for all routes
+
+app.use(cors()); 
 app.use(express.json());
 
 app.post('/contact', (req, res) => {
   const { name, email, subject, message } = req.body;
 
-  // Perform whatever processing you need to do here
+  
   console.log("Received contact form data:", req.body);
 
-  // Send a success response
+ 
   res.json({ message: "Thank you for reaching out. I'll get back to you soon!" });
 });
 
